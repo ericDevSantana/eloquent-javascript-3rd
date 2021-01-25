5bd70c5368c34daa4494f22fd9718aa4e9169741
Eric <22939634+ericDevSantana@users.noreply.github.com>
2021-01-24 23:23:29 -0800

    Final Commit

--- chapter-5/exercise1.js
+++ chapter-5/exercise1.js
@@ -1,1 +1,12 @@
-// This is my first commit
+// Array of arrays to be flattened
+let myArray = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];
+
+// Reduce method:
+// input = array of arrays
+// output = one array containing all elements of all arrays inside input's array
+let flattened = myArray.reduce(function (accumulator, currentItem) {
+    return accumulator.concat(currentItem);
+}, []);
+
+// Show result
+console.log(flattened);
